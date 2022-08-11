c448915948aec8da05be4f93b940879ec12dca19
sabutaz <110493078+sabutaz@users.noreply.github.com>
2022-08-11 18:24:41 +0300

    file 21 changes

--- 21.py
+++ 21.py
@@ -7,7 +7,8 @@ my_driver.find_element(by="xpath", value='//*[@id="serviceQual"]/option[4]').cli
 my_driver.find_element(by="xpath", value='//*[@id="peopleamt"]').send_keys("5")
 my_driver.find_element(by="xpath", value='//*[@id="music"]').send_keys("5")
 my_driver.find_element(by="xpath", value='//*[@id="calculate"]').click()
-expected = "8.00"
+expected = "10.00"
+
 actual = my_driver.find_element(by="id", value="tip").text
 
 assert expected != actual
