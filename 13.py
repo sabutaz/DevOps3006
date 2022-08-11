34f6e7ab0692c1fba1c55bc8110c1b61fed59f5b
sabutaz <110493078+sabutaz@users.noreply.github.com>
2022-08-11 20:06:19 +0300

    Update 13.py

--- 13.py
+++ 13.py
@@ -5,6 +5,10 @@ def save_name(the_file):
     my_file.close()
 
 
+def haim(avi):
+    print(avi)
+    
+
 def show_names(the_file):
     my_file = open(the_file, "r")
     for name in my_file.readlines():
