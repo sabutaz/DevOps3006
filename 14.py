361f641b69ffa954efb65b9cb85b9fd3eaa017da
sabutaz <110493078+sabutaz@users.noreply.github.com>
2022-08-11 20:15:51 +0300

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,7 +1,7 @@
 import ast
 my_file = open("config.json")
 c = dict(ast.literal_eval(my_file.read()))
-if c["name"] == "saby":
+if c["name"] == "sababa":
     print("I love DevOps")
 
 with open("names.txt") as my_file:
